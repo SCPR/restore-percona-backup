643dc5dc34358dc85814633f9e494fdeb3da9bb6
Eric Richardson <e@ericrichardson.com>
2016-02-12 16:07:59 -0500

    Only run our backup fetch the first time
    Allows a container to be stopped and restarted without having to fetch the
    backup again or losing state.

--- restore-backup.sh
+++ restore-backup.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 set -e
 
-mkdir -p /var/lib/mysql
-restore-percona-backup $1
-chown -R mysql:mysql /var/lib/mysql
-mysql_install_db --datadir=/var/lib/mysql --user=mysql
+if [ -e /.backup-restored ];
+then
+  echo
+  echo 'Starting MySQL using prepared backup.'
+  echo
+else
+  mkdir -p /var/lib/mysql
+  restore-percona-backup $1
+  chown -R mysql:mysql /var/lib/mysql
+  mysql_install_db --datadir=/var/lib/mysql --user=mysql
 
-mysqld &
-pid="$!"
+  mysqld &
+  pid="$!"
 
-mysql=( mysql --protocol=socket -uroot )
-for i in {30..0}; do
-	if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
-		break
-	fi
-	echo 'MySQL init process in progress...'
-	sleep 1
-done
-if [ "$i" = 0 ]; then
-	echo >&2 'MySQL init process failed.'
-	exit 1
+  mysql=( mysql --protocol=socket -uroot )
+  for i in {30..0}; do
+  	if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+      break
+    fi
+    echo 'MySQL init process in progress...'
+    sleep 1
+  done
+  if [ "$i" = 0 ]; then
+    echo >&2 'MySQL init process failed.'
+    exit 1
+  fi
+
+  echo "GRANT ALL PRIVILEGES ON *.* to root@`%` WITH GRANT OPTION;" | "${mysql[@]}"
+  echo "FLUSH PRIVILEGES;" | "${mysql[@]}"
+
+  if ! kill -s TERM "$pid" || ! wait "$pid"; then
+    echo >&2 'MySQL init process failed.'
+    exit 1
+  fi
+
+  touch /.backup-restored
+
+  echo
+  echo 'MySQL init process done. Ready for start up.'
+  echo
 fi
 
-echo "GRANT ALL PRIVILEGES ON *.* to root@`%` WITH GRANT OPTION;" | "${mysql[@]}"
-echo "FLUSH PRIVILEGES;" | "${mysql[@]}"
-
-wait $pid
+exec "mysqld"
